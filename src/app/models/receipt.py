b9da5e468c12045de8bb869a31629dbe9630bea5
Koch Viktor <viktor.koch@oxit.hu>
2024-04-20 22:23:08 +0200

    Add receipt statuses

--- src/app/models/receipt.py
+++ src/app/models/receipt.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..core.db.database import Base
@@ -9,9 +11,20 @@ from ..core.models.mixins import (
 )
 
 
+class ReceiptStatus(Enum):
+    PENDING = "pending"
+    APPROVED = "approved"
+    REJECTED = "rejected"
+    PROCESSING = "processing"
+    PROCESSED = "processed"
+
+
 class Receipt(
     IDMixin, UUIDMixin, TimestampMixin, SoftDeleteMixin, Base, kw_only=True
 ):
     url: Mapped[str] = mapped_column()
     title: Mapped[str] = mapped_column()
     description: Mapped[str | None] = mapped_column()
+    status: Mapped[ReceiptStatus] = mapped_column(
+        index=True, default=ReceiptStatus.PENDING
+    )
