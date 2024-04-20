57ee3ac94db06c23489359f7d3789acf35a3775c
Koch Viktor <viktor.koch@oxit.hu>
2024-04-20 22:25:44 +0200

    Add receipt and tag to __init__ file

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -2,6 +2,8 @@ from .group import Group  # noqa: F401
 from .income import Income  # noqa: F401
 from .purchase_category import PurchaseCategory  # noqa: F401
 from .rate_limit import RateLimit  # noqa: F401
+from .receipt import Receipt  # noqa: F401
+from .tag import Tag  # noqa: F401
 from .tier import Tier  # noqa: F401
 from .transaction import Transaction  # noqa: F401
 from .transaction_item import TransactionItem  # noqa: F401
@@ -13,6 +15,8 @@ __all__ = [
     "Income",
     "PurchaseCategory",
     "RateLimit",
+    "Receipt",
+    "Tag",
     "Tier",
     "Transaction",
     "TransactionItem",
