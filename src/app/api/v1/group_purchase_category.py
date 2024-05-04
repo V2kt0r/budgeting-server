13113dc708705b454a912af9c6971076be71e0fb
Koch Viktor <viktor.koch@oxit.hu>
2024-05-04 19:59:28 +0200

    Add GET /group/{group_uuid}/purchase-category endpoint.

--- src/app/api/v1/group_purchase_category.py
+++ src/app/api/v1/group_purchase_category.py
@@ -148,3 +148,62 @@ async def create_group_purchase_category(
     )
 
     return purchase_category_model
+
+
+@router.get(
+    "/group/{group_uuid}/purchase-category",
+    response_model=PaginatedListResponse[PurchaseCategoryRead],
+)
+async def get_group_purchase_categories(
+    *,
+    request: Request,
+    group_uuid: Annotated[
+        uuid_pkg.UUID,
+        Path(
+            description="UUID of the group to which the purchase categories belong.",
+            examples=[uuid_pkg.uuid4(), uuid_pkg.uuid4(), uuid_pkg.uuid4()],
+        ),
+    ],
+    current_user: Annotated[UserSchema, Depends(get_current_user)],
+    db: Annotated[AsyncSession, Depends(async_get_db)],
+    page: int = Query(ge=1, default=1),
+    items_per_page: int = Query(ge=1, le=100, default=10),
+) -> Any:
+    # Check if group exists
+    group_exists: bool = await crud_groups.exists(
+        db=db, uuid=group_uuid, is_deleted=False
+    )
+    if not group_exists:
+        raise NotFoundException("The group with this UUID does not exist.")
+
+    # Check if user is a member of the group
+    user_is_member: bool = await crud_group_user.exists(
+        db=db,
+        user_id=current_user.id,
+        group_uuid=group_uuid,
+    )
+    if not user_is_member:
+        raise ForbiddenException(
+            "You are not a member of the group with this UUID."
+        )
+
+    # Get the purchase categories
+    group_purchase_category_join_config = JoinConfig(
+        model=GroupPurchaseCategoryModel,
+        join_on=(
+            GroupPurchaseCategoryModel.purchase_category_id
+            == PurchaseCategoryModel.id
+        ),
+        schema_to_select=BaseModel,
+        filters={"group_uuid": group_uuid},
+    )
+    crud_data: dict[str, Any] = await crud_purchase_categories.get_multi_joined(
+        db=db,
+        is_deleted=False,
+        joins_config=[group_purchase_category_join_config],
+        return_as_model=True,
+        schema_to_select=PurchaseCategoryRead,
+    )
+    return paginated_response(
+        crud_data=crud_data, page=page, items_per_page=items_per_page
+    )
