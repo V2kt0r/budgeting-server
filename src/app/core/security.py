36047a54fd8025024d2760785b56e737e8ba5d55
Koch Viktor <viktor.koch@oxit.hu>
2024-05-20 21:36:41 +0200

    Add new login endpoint for swagger

--- src/app/core/security.py
+++ src/app/core/security.py
@@ -17,7 +17,7 @@ ALGORITHM = settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 REFRESH_TOKEN_EXPIRE_DAYS = settings.REFRESH_TOKEN_EXPIRE_DAYS
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login-swagger")
 
 
 async def verify_password(plain_password: str, hashed_password: str) -> bool:
